67c0cb75e4591701f91b2b859ef4a0bd8c61775a
Addi <coxonmarc1@gmail.com>
2020-09-14 14:28:21 +0700

    added register route

--- routes/userApi.js
+++ routes/userApi.js
@@ -1,4 +1,4 @@
-/** 
+/**
  * @route POST api/users - Register
  * @route GET api/users?page=1&limit=20
  * @route GET api/users/me - Get current user
@@ -8,10 +8,30 @@
  * @route PUT api/users/me/password - Change Password
  */
 
- /**
-  * @route POST api/users - Register
-  * @description Register a new user
-  * @access Public
-  */
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/user.controller.js");
+const validators = require("../middlewares/validators.js");
+const { body } = require("express-validator");
+const authMiddleware = require("../middlewares/authentication.js");
 
-router.post('/register', userController.register);
+/**
+ * @route POST api/users 
+ * @description Register a new user
+ * @access Public
+ */
+
+router.post(
+  "/register",
+  validators.validate([
+    body("surname", "Surname required").exists().notEmpty(),
+    body("firstName", "First name required").exists().notEmpty(),
+    body("email", "Email required").exists().notEmpty(),
+    body("email", "Invalid email").exists().isEmail(),
+    body("password", "Password required").exists().notEmpty(),
+    body("password", "Password must be at least 6 characters long")
+      .exists()
+      .isLength(6),
+  ]),
+  userController.register
+);
