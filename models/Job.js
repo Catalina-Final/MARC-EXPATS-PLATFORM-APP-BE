0c179dc1624e10b4fb3c817aad757f54b679a6d8
Addi <coxonmarc1@gmail.com>
2020-09-18 11:40:31 +0700

    add job schema

---
+++ models/Job.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const JobSchema = Schema({
+  jobOwner: { type: Schema.Types.ObjectId, required: false, ref: "Users" },
+  jobOverview: Schema({
+    jobTitle: { type: String, required: false },
+    salary: { type: String, required: false },
+    location: { type: String, required: false },
+    contractType: { type: String, required: false },
+    employer: { type: String, required: false },
+    startDate: { type: String, required: false },
+  }),
+  jobDetails: Schema({
+    description: { type: String, required: false },
+    requiredQualifactions: { type: String, required: false },
+    requiredSkills: { type: String, required: false },
+    requiredCharacteristics: { type: String, required: false },
+    incentives: { type: String, required: false },
+    bonuses: { type: String, required: false },
+  }),
+  jobBannerImage: Schema({ type: String, required: false }),
+});
+
+const Job = mongoose.model("Job", JobSchema);
+module.exports = Job;
